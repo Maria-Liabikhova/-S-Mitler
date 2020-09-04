f2d7018500ea3b2e6ab2a175d440302c37b7d1f0
unknown <mashagi@yandex.ru>
2020-09-04 16:03:30 +0300

    problems with hamburger menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
+const { logError } = require("gulp-sass");
+
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: [ 'widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: '<img src="img/logo.svg" alt="Салон красоты">'
+		},
+		offCanvas: {
+			position: right
+		}
+	});
 
 });
