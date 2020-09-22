a181668b7d8efef6633ef3bf724f38ea77ef025b
unknown <mashagi@yandex.ru>
2020-09-22 09:42:02 +0300

    problem: mmenu on the left side

--- app/js/common.js
+++ app/js/common.js
@@ -2,25 +2,29 @@
 
 $(function() {
 
-// 	$('#my-menu').mmenu({
-// 		extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black'],
-// 		navbar: {
-// 			title: '<img src="img/logo.svg" alt="Салон красоты">'
-// 		},
-// 		offCanvas: {
-// 			position: "right"
-// 		}
-// 	});
+	$('#my-menu').mmenu({
+		extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: '<img src="img/logo.svg" alt="Салон красоты">'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
 
-// 	let api = menu.API.data('mmenu');
-// 	let api = $('my-menu').data('mmenu');
-// 	api.bind('openPanel:start', function(){
-// 		$('.hamburger').addClass('is-active');
-// 	});
-// 	api.bind('closePanel', function(){
-// 		$('.hamburger').removeClass('is-active');
-// 	})
+	let api = $('#my-menu').data('mmenu');
+	api.bind('openPanel:start', function(){
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind('closePanel:before', function(){
+		$('.hamburger').removeClass('is-active');
+	})
 
+
+
+
+
+  
   $('.carousel-services').on('initialized.owl.carousel', function() {
     setTimeout(function() {
       carouselService()
